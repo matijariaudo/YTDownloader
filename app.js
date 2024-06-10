dcd7a70939a8cbecf7bdea2a38ed1c206ba25f10
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2024-06-10 23:43:25 +1000

    cambios qr

--- app.js
+++ app.js
@@ -10,20 +10,25 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, './public')));
 
 app.post('/api', async(req, res)=>{
-    const {remoteJid,conversation}=req.body;
-    if(remoteJid=='5493406460886@s.whatsapp.net'){
-        if(conversation.indexOf('youtube')){
-            const code=extractVideoId(conversation)
-            console.log("codigo:",code)
-            if(!code){send("5493406460886@s.whatsapp.net",`I can't send you de audio :(`);return false;}
-            console.log("Seguimos")
-            const url=await sendVideo(code)
-            console.log(url)
-            const baseUrl = `${req.protocol}://${req.get('host')}`;
-            await sendMedia("5493406460886@s.whatsapp.net",`I send to you the audio`,baseUrl+url.urlFile,url.name)
-            eliminarCarpeta(url.auxName);
-        }
+    try {
+        const {remoteJid,conversation}=req.body;
+        if(remoteJid=='5493406460886@s.whatsapp.net'){
+            if(conversation.indexOf('youtube')){
+                const code=extractVideoId(conversation)
+                console.log("codigo:",code)
+                if(!code){send("5493406460886@s.whatsapp.net",`I can't send you de audio :(`);return false;}
+                console.log("Seguimos")
+                const url=await sendVideo(code)
+                console.log(url)
+                const baseUrl = `${req.protocol}://${req.get('host')}`;
+                await sendMedia("5493406460886@s.whatsapp.net",`I send to you the audio`,baseUrl+url.urlFile,url.name)
+                eliminarCarpeta(url.auxName);
+            }
+        }    
+    } catch (error) {
+        
     }
+    return res.json({msg: "Muchas gracias"})
 })
 app.get('/api',async(req,res)=>{
     const url=await sendVideo("lGNwnstqAO4")
@@ -34,11 +39,8 @@ app.get('/api',async(req,res)=>{
 })
 
 app.get('/test',async(req,res)=>{
-    //const url=await sendVideo("lGNwnstqAO4")
-    //const baseUrl = `${req.protocol}://${req.get('host')}`;
     send("5493406460886@s.whatsapp.net",`⚠️ New supplier has arrived.`)
-    return res.json({a:"a"})
-
+    return res.redirect(`mensaje.html`);
 })
 
 const port=process.env.PORT|| 8000;
